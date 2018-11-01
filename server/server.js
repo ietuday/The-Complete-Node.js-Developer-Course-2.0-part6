cf554e2ad69879187fb537c8f998a0dbeb09d74c
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-01 19:21:37 +0530

    Welcome message and broadcast

--- server/server.js
+++ server/server.js
@@ -17,6 +17,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to Chat App',
+        cretedAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined',
+        cretedAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage',message);
         io.emit('newMessage', {
@@ -24,6 +36,11 @@ io.on('connection', (socket) => {
             text: message.text,
             cretedAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     cretedAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', function () {
