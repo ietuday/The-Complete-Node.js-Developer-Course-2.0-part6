35005892450bae2cc6105e280e720d0bbcf29204
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-01 18:44:57 +0530

    brodcasting

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,15 @@
 var socket = io();
 
 socket.on('connect', function () {
-    console.log('Connected to the server');
+    console.log('Connected to the server'); 
+});
 
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'Yup, that work for me'
-    });
+socket.on('newMessage', function(message){
+    console.log('newMessage',message);
+});
 
-    socket.on('newMessage', function(message){
-        console.log('newMessage',message);
-    });
-
-    socket.on('disconnect', function(){
-        console.log('Disconnect from server');
-    })
+socket.on('disconnect', function(){
+    console.log('Disconnect from server');
 });
 
 
--- server/server.js
+++ server/server.js
@@ -17,14 +17,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'See you then',
-        cretedAt: 12312
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage',message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            cretedAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', function () {
