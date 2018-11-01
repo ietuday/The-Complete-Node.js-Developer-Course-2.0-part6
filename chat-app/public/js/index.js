14bda87fb16b39833e8865247ad2edab516c872b
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-01 19:26:02 +0530

    Real time chat app with socket.io

---
+++ chat-app/public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to the server'); 
+});
+
+socket.on('newMessage', function(message){
+    console.log('newMessage',message);
+});
+
+socket.on('disconnect', function(){
+    console.log('Disconnect from server');
+});
+
+
+
+
+
+
