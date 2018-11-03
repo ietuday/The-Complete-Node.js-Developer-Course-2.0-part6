5da5162ca71c337cdfca705015f9110e8213772b
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-03 14:47:31 +0530

    chat romm created with dynamic data

--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage, generateLocationMessage} = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
+const { isRealString } = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -15,9 +16,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and room are required');
+    }
+    socket.join(params.room);
+    //io.emit: Emits to every connected users. 
+    //socket.brodcast.emit: emits to every connected servers except for the current user.
+    //socket.emit:Emits to specific to one user.
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    // socket.leave('The Office Fans');
+
+    // io.emit -> io.to('The Office Fans').emit
+    // socket.broadcast.emit -> socket.broadcast.to('The Office Fans').emit
+    // socket.emit
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
--- chat-app/server/utils/message.test.js
+++ chat-app/server/utils/message.test.js
@@ -1,31 +1,27 @@
-const expect = require('expect');
-const { generateMessage,  generateLocationMessage } = require('./message');
+var expect = require('expect');
+
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-    it('should generate correct message object', () => {
+  it('should generate correct message object', () => {
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
 
-        var from = 'Jen';
-        var text = 'Some message';
-        var message = generateMessage(from, text);
-
-        expect(typeof message.createdAt).toBe('number');
-        expect(message).toMatchObject({ from, text });
-    });
-
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
 });
 
 describe('generateLocationMessage', () => {
-    it('should generate correct location object', () => {
-        var from = 'Deb';
-        var latitude = 15;
-        var longitude = 19;
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
 
-        var url = `https://www.google.com/maps?q=15,19`;
-
-        var message = generateLocationMessage(from, latitude, longitude);
-
-        expect(typeof message.createdAt).toBe('number');
-        expect(message).toMatchObject({ from, url });
-
-    });  
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
 });
